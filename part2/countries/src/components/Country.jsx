b8201e5a3b42a78af78b4f38c55281dac55f9b78
Chyisiang <chyisiang8@gmail.com>
2023-11-22 22:08:41 +0800

    2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    const lat = country.capitalInfo.latlng[0];
+    const lon = country.capitalInfo.latlng[1];
+    const API_KEY = import.meta.env.VITE_SOME_KEY;
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [country]);
+
+  console.log(weather);
+
   return (
     <div key={country.cca3}>
       <h2>{country.name.common}</h2>
@@ -10,6 +31,17 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} />
+      {weather && weather.main && weather.wind && (
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <p>temperature: {weather.main.temp} K</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather.description}
+          />
+          <p>wind: {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
 };
