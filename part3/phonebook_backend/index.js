3f9270bb3ae3a0a400c353d8d7935e4248e7497b
Chyisiang <chyisiang8@gmail.com>
2023-11-23 21:27:51 +0800

    3.7-3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 let persons = [
   {
