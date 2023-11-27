229ddd13073e26fe1c4376d8e880cd7218f72aa6
Chyisiang <chyisiang8@gmail.com>
2023-11-27 21:57:19 +0800

    4.15 - 4.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const config = require("./utils/config");
-require('express-async-errors')
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ part4/bloglist/tests/user_api.test.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const helper = require("./test_helper");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const User = require("../models/user");
+
+test("invalid users are not created", async () => {
+  const newUser = {
+    username: "ab",
+    name: "Test User",
+    password: "12",
+  };
+
+  await api
+    .post("/api/users")
+    .send(newUser)
+    .expect(400)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/users");
+  expect(response.body).toHaveLength(0);
+});
