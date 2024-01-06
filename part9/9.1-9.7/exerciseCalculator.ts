b2c7637a2df00ce8cc4bfde67ad335a6db4ed0a9
Chyisiang <chyisiang8@gmail.com>
2024-01-06 13:01:31 +0800

    9.2

---
+++ part9/9.1-9.7/exerciseCalculator.ts
@@ -0,0 +1,41 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (dailyHours: number[], target: number): Result => {
+  const periodLength = dailyHours.length;
+  const trainingDays = dailyHours.filter((hour) => hour > 0).length;
+  const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength;
+  const success = average >= target;
+  let rating;
+  let ratingDescription;
+
+  if (average < target / 2) {
+    rating = 1;
+    ratingDescription = "you need to work harder";
+  } else if (average < target) {
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
+  } else {
+    rating = 3;
+    ratingDescription = "well done!";
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
