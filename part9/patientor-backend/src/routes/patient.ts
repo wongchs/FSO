df4ada81e85e89a4f92e7305090ec81456370ef0
Chyisiang <chyisiang8@gmail.com>
2024-01-08 12:26:08 +0800

    9.13

--- part9/patientor-backend/src/routes/patient.ts
+++ part9/patientor-backend/src/routes/patient.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import patientService from "../services/patientService";
+import toNewPatientEntry from "../utils";
 
 const patientRouter = express.Router();
 
@@ -8,16 +9,18 @@ patientRouter.get("/", (_req, res) => {
 });
 
 patientRouter.post("/", (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  try {
+    const newPatientEntry = toNewPatientEntry(req.body);
 
-  const addedPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-  res.json(addedPatient);
+    const addedPatient = patientService.addPatient(newPatientEntry);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default patientRouter;
