f9ca14eef03e9b266c9fb2a1a69a6a0a4f8940f4
Chyisiang <chyisiang8@gmail.com>
2024-01-09 21:23:15 +0800

    9.17

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { DiaryEntry } from "./types";
-import { getDiaries } from "./services/diaryService";
+import { addDiary, getDiaries } from "./services/diaryService";
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [newDiary, setNewDiary] = useState("");
+  const [date, setDate] = useState("");
+  const [weather, setWeather] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     getDiaries().then((data) => {
@@ -11,9 +16,54 @@ function App() {
     });
   });
 
+  const createDiary = (event) => {
+    event.preventDefault();
+    const newDiary = {
+      date,
+      weather,
+      visibility,
+      comment,
+    };
+    addDiary(newDiary).then((addedDiary) => {
+      setDiaries(diaries.concat(addedDiary));
+    });
+  };
+
   return (
     <>
       <div>
+        <form onSubmit={createDiary}>
+          <p>
+            date{" "}
+            <input
+              type="date"
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+            />
+          </p>
+          <p>
+            weather{" "}
+            <input
+              value={weather}
+              onChange={(event) => setWeather(event.target.value)}
+            />
+          </p>
+          <p>
+            visibility{" "}
+            <input
+              value={visibility}
+              onChange={(event) => setVisibility(event.target.value)}
+            />
+          </p>
+          <p>
+            comment{" "}
+            <input
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+            />
+          </p>
+          <button type="submit">add</button>
+        </form>
         <h2>diary entries</h2>
         <ul>
           {diaries.map((diary) => (
--- part9/flight-diary-frontend/src/services/diaryService.ts
+++ part9/flight-diary-frontend/src/services/diaryService.ts
@@ -6,3 +6,9 @@ const baseUrl = "http://localhost:3000/api/diaries";
 export const getDiaries = () => {
   return axios.get<DiaryEntry[]>(baseUrl).then((response) => response.data);
 };
+
+export const addDiary = (newDiary: Omit<DiaryEntry, "id">) => {
+  return axios
+    .post<DiaryEntry>(baseUrl, newDiary)
+    .then((response) => response.data);
+};
