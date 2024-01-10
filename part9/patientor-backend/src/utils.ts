ff7cd245cfa5306b8182b5c57c526a531a4b0713
Chyisiang <chyisiang8@gmail.com>
2024-01-10 10:51:18 +0800

    update

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -70,6 +70,7 @@ const toNewPatientEntry = (object: unknown): NewPatientEntry => {
       ssn: parseSsn(object.ssn),
       gender: parseGender(object.gender),
       occupation: parseOccupation(object.occupation),
+      entries: [],
     };
 
     return newEntry;
