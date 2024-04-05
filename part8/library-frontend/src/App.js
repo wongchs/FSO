f2bdc283ef00fb5c729d0f67017b0b1c509a7ee7
Chyisiang <chyisiang8@gmail.com>
2024-04-05 21:31:32 +0800

    8.10

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -26,6 +26,22 @@ query {
 }
 `
 
+export const CREATE_BOOK = gql`
+mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  addBook(
+    title: $title,
+    author: $author,
+    published: $published,
+    genres: $genres
+  ) {
+    title
+    published
+    author
+    genres
+  }
+}
+`
+
 const App = () => {
   const [page, setPage] = useState('authors')
 
